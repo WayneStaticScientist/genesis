bce644420ffeee320e208ba6d96a2aeb9d6a365f
WayneStaticScientist <vanderlinkinchat@gmail.com>
2026-02-08 08:30:59 +0200

    sffr

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,4 @@
-import java.util.Properties 
 
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localPropertiesFile.inputStream().use { localProperties.load(it) }
-}
-
-// 2. Extract the key
-val mapsApiKey: String = localProperties.getProperty("MAPS_API_KEY") ?: ""
 
 allprojects {
     repositories {
@@ -32,11 +23,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-android {
-
-    defaultConfig {
-        manifestPlaceholders["mapsApiKey"] = mapsApiKey
-    }
 }
